d9311e870b8715bee264cd1ade62d40e871e72d1
seanHolahan <holahan.s@husky.neu.edu>
2018-07-10 16:00:05 -0400

    fancy background skater slide

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,11 +13,56 @@ import { fadeInAnimation } from '../app-animations/index';
 })
 
 
+
 export class HomeComponent implements OnInit {
 
+  innerHeight: any;
+  innerWidth: any;
+  image =  "../../assets/img/skate2.png";
+  skate2Opacity = 0;
+  skate3Opacity = 0;
+
+  constructor() {
+    this.innerHeight = (window.screen.height) + "px";
+    this.innerWidth = (window.screen.width) + "px";
+    console.log(innerHeight, innerWidth);
+  }
+
+  event: MouseEvent;
+  clientX = 0;
+  clientY = 0;
+
+  onEvent(event: MouseEvent): void {
+    this.event = event;
+  }
+
+  coordinates(event: MouseEvent): void {
+  this.clientX = event.clientX;
+  this.clientY = event.clientY;
+    if(this.clientX / innerWidth < .33) {
+      console.log("bloop");
+      this.image ="../../assets/img/skate2.png";
+      this.skate2Opacity = 0;
+      this.skate3Opacity = 0;
+    }
+    if(this.clientX / innerWidth > .33) {
+      console.log("bloop");
+      this.image ="../../assets/img/skate2.png";
+      this.skate2Opacity = 100;
+      this.skate3Opacity = 0;
+
+    }
+    if(this.clientX / innerWidth > .66) {
+      console.log("geep");
+      this.image ="../../assets/img/skate2.png";
+      this.skate2Opacity = 100;
+      this.skate3Opacity = 100;
+    }
+  }
 
 
-  constructor() { }
+
   ngOnInit() {
+
   };
 }
